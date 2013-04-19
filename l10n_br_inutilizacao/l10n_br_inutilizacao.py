3b6d238be4916d7612ed687ac2cef05fd04bb4e4
Danimar Ribeiro <danimaribeiro@gmail.com>
2013-04-18 23:56:50 -0300

    Implementado envio da inutilização.

--- l10n_br_inutilizacao/l10n_br_inutilizacao.py
+++ l10n_br_inutilizacao/l10n_br_inutilizacao.py
@@ -2,12 +2,18 @@
 from openerp.osv import osv, fields 
 import pysped.nfe
 import base64
+import re
 
 class l10n_br_account_invoice_invalid_number(osv.Model):
     _inherit = 'l10n_br_account.invoice.invalid.number'
-
+    #TODO Impedir de mudar os valoes após o status done
     _columns = {
+            'state': fields.selection(
+            [('draft', 'Rascunho'), ('not_authorized', 'Não autorizado'),
+            ('done', u'Autorizado Sefaz')], 'Status', required=True),
             'justificative': fields.char('Justificativa', size=255,required=True),
+            'status':fields.char('Status', size=10),
+            'message':fields.char('Mensagem', size=200),
         }
     def _check_justificative(self, cr,uid, ids):
         for invalid in self.browse(cr, uid, ids):
@@ -17,9 +23,18 @@ class l10n_br_account_invoice_invalid_number(osv.Model):
     _constraints = [(_check_justificative,'Justificativa deve ter tamanho minimo de 15 caracteres.', ['justificative'])]
     
 
-    def action_draft_done(self, cr, uid, ids, *args):        
-        self.send_request_to_sefaz(cr, uid, ids , args)
-        self.write(cr, uid, ids, {'state': 'done'})
+    def action_draft_done(self, cr, uid, ids, *args):
+        try:
+            processo = self.send_request_to_sefaz(cr, uid, ids , args)
+            
+            if processo.resposta.infInut.cStat.valor == '102':
+                self.write(cr, uid, ids, {'state': 'done', 'status':'102', 'message':processo.resposta.infInut.xMotivo.valor})
+            else:
+                self.write(cr, uid, ids, {'state':'not_authorized', 'status':processo.resposta.infInut.cStat.valor,
+                        'message':processo.resposta.infInut.xMotivo.valor})
+                
+        except Exception,e:        
+            self.write(cr, uid, ids, {'justificative': e.message})
         return True
     
     def send_request_to_sefaz(self, cr, uid, ids, *args):
@@ -29,7 +44,7 @@ class l10n_br_account_invoice_invalid_number(osv.Model):
         company = company_pool.browse(cr, uid, record.company_id.id)
                 
         p = pysped.nfe.ProcessadorNFe()
-        p.versao = company.nfe_version
+        p.versao = '2.00' if (company.nfe_version == '200') else '1.10'
         p.estado = company.partner_id.l10n_br_city_id.state_id.code
         
         file_content_decoded = base64.decodestring(company.nfe_a1_file)
@@ -41,7 +56,7 @@ class l10n_br_account_invoice_invalid_number(osv.Model):
         p.certificado.arquivo = filename
         p.certificado.senha = company.nfe_a1_password
     
-        p.salva_arquivos = False
+        p.salva_arquivos = True
         p.contingencia_SCAN = False
         p.caminho = company.nfe_export_folder
     
@@ -55,20 +70,17 @@ class l10n_br_account_invoice_invalid_number(osv.Model):
         # .resposta.status - status da HTTPResponse
         # .resposta.reason - reason da HTTPResponse
         # .resposta.msg - msg da HTTPResponse
-        # .resposta.original - o texto do xml (SOAP) recebido do webservice
-        
-        cnpj_partner = company.partner_id.cnpj_cpf
+        # .resposta.original - o texto do xml (SOAP) recebido do webservice                
+        cnpj_partner = re.sub('[^0-9]','', company.partner_id.cnpj_cpf)
         serie = record.document_serie_id.code
-                
-        # Inutilizar somente uma nota     
+
         processo = p.inutilizar_nota(
             cnpj=cnpj_partner,
             serie=serie,
             numero_inicial=record.number_start,
             numero_final=record.number_end,
             justificativa=record.justificative)
-    
-        print processo.resposta.status
-        print processo.resposta.reason
-        cr.sql_log = False
+                
+        cr.sql_log = False            
+        return processo
         
