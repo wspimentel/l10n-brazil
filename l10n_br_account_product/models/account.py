b3749a1de94c87a7dc6a716f7d4124b8a17bf58b
Hendrix Costa <hendrixcosta@gmail.com>
2016-08-17 21:24:34 -0300

    [FIX] Code style

--- l10n_br_account_product/models/account.py
+++ l10n_br_account_product/models/account.py
@@ -333,9 +333,9 @@ class AccountTax(models.Model):
 
         # Estimate Taxes
         if fiscal_position and fiscal_position.asset_operation:
-                total_taxes = ((result['total_included'] - totaldc) *
-                               product.product_estimated_taxes_percent/100)
-                result['total_taxes'] = round(total_taxes, precision)
+            total_taxes = ((result['total_included'] - totaldc) *
+                           product.product_estimated_taxes_percent/100)
+            result['total_taxes'] = round(total_taxes, precision)
 
         return {
             'total': result['total'],
