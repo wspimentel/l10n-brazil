akretion/l10n_brazil

Added CEST in NFe serialization

l10n_br_account_product/sped/nfe/document.py
@@ -344,6 +344,8 @@ class NFe200(FiscalDocument):
             invoice_line.fiscal_classification_id.code or '')[:8]
         self.det.prod.EXTIPI.valor = punctuation_rm(
             invoice_line.fiscal_classification_id.code or '')[8:]
+        self.det.prod.CEST.valor = punctuation_rm(
+            invoice_line.cest_id.id or '')
         self.det.prod.nFCI.valor = invoice_line.fci or ''
         self.det.prod.CFOP.valor = invoice_line.cfop_id.code
         self.det.prod.uCom.valor = invoice_line.uos_id.name or ''

wspimentel/l10n-brazil

l10n_br_account_product/sped/nfe/document.py
@@ -344,6 +344,8 @@ class NFe200(FiscalDocument):
             invoice_line.fiscal_classification_id.code or '')[:8]
         self.det.prod.EXTIPI.valor = punctuation_rm(
             invoice_line.fiscal_classification_id.code or '')[8:]
+        self.det.prod.CEST.valor = punctuation_rm(
+            invoice_line.cest_id.code or '')
         self.det.prod.nFCI.valor = invoice_line.fci or ''
         self.det.prod.CFOP.valor = invoice_line.cfop_id.code
         self.det.prod.uCom.valor = invoice_line.uos_id.name or ''