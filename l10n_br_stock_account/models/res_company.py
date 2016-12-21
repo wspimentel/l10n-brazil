6755d007a16cebd13c5ec737fa4880f57bcc67d8
Luis Felipe Mileo <mileo@kmee.com.br>
2016-12-21 15:41:05 -0200

    [NEW] Intercompany stock transfers configurations

--- l10n_br_stock_account/models/res_company.py
+++ l10n_br_stock_account/models/res_company.py
@@ -21,3 +21,33 @@ class ResCompany(models.Model):
         u'Categoria Fiscal Padrão Saída',
         domain="[('journal_type', 'in', ('purchase_refund', 'sale')), "
         "('fiscal_type', '=', 'product'), ('type', '=', 'output')]")
+    stock_out_resupply_fiscal_category_id = fields.Many2one(
+        'l10n_br_account.fiscal.category',
+        u'Categoria Fiscal Padrão de saída de transferencias entre filiais',
+        domain="[('journal_type', 'in', ('purchase_refund', 'sale')),"
+               "('fiscal_type', '=', 'product'), ('type', '=', 'output')]")
+    stock_out_resupply_invoice_state = fields.Selection(
+        selection=[
+            ("invoiced", "Invoiced"),
+            ("2binvoiced", "To Be Invoiced"),
+            ("none", "Not Applicable")
+        ],
+        string=u"Tipo de faturamento padrão de saída",
+        help=u"""Define o tipo de faturamento padrão aplicável nas operaçoes de
+        de envio, saindo desta empresa para outra filial"""
+    )
+    stock_in_resupply_fiscal_category_id = fields.Many2one(
+        'l10n_br_account.fiscal.category',
+        u'Categoria Fiscal Padrão de entrada de transferencias entre filiais',
+        domain="[('journal_type', 'in', ('sale_refund', 'purchase')), "
+               "('fiscal_type', '=', 'product'), ('type', '=', 'input')]")
+    stock_in_resupply_invoice_state = fields.Selection(
+        selection=[
+            ("invoiced", "Invoiced"),
+            ("2binvoiced", "To Be Invoiced"),
+            ("none", "Not Applicable")
+        ],
+        string=u"Tipo de faturamento padrão de entrada",
+        help=u"""Define o tipo de faturamento padrão aplicável nas operaçoes de
+        de envio, saindo desta empresa para outra filial"""
+    )
