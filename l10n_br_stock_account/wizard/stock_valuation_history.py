877dec27909ee9269f17ab7efc03398472dd067b
sadamo <daniel.sadamo@kmee.com.br>
2017-02-15 14:09:55 -0200

    [fix] price_unit_on_quant in stock_history report

--- l10n_br_stock_account/wizard/stock_valuation_history.py
+++ l10n_br_stock_account/wizard/stock_valuation_history.py
@@ -19,6 +19,7 @@
 #
 ##############################################################################
 
+from datetime import datetime
 from openerp import models, api, _
 from openerp.osv import osv, fields
 
@@ -92,9 +93,75 @@ class StockHistory(osv.osv):
 
     def read_group(self, cr, uid, domain, fields, groupby, offset=0,
                    limit=None, context=None, orderby=False, lazy=True):
-        res = super(StockHistory, self).read_group(
-            cr, uid, domain, fields, groupby, offset=offset, limit=limit,
-            context=context, orderby=orderby, lazy=lazy)
+        res = models.Model.read_group(self, cr, uid, domain, fields, groupby,
+                                      offset=offset, limit=limit,
+                                      context=context, orderby=orderby,
+                                      lazy=lazy)
+        if context is None:
+            context = {}
+        date = context.get('history_date', datetime.now())
+        if 'inventory_value' in fields:
+            group_lines = {}
+            for line in res:
+                domain = line.get('__domain', domain)
+                group_lines.setdefault(
+                    str(domain), self.search(cr, uid, domain, context=context))
+            line_ids = set()
+            for ids in group_lines.values():
+                for product_id in ids:
+                    line_ids.add(product_id)
+            line_ids = list(line_ids)
+            lines_rec = {}
+            if line_ids:
+                cr.execute('SELECT id, product_id, price_unit_on_quant,'
+                           ' company_id, quantity FROM stock_history '
+                           'WHERE id in %s', (tuple(line_ids),))
+                lines_rec = cr.dictfetchall()
+            lines_dict = dict((line['id'], line) for line in lines_rec)
+            product_ids = list(
+                set(line_rec['product_id'] for line_rec in lines_rec))
+            products_rec = self.pool['product.product'].read(
+                cr, uid, product_ids,
+                ['cost_method', 'product_tmpl_id'], context=context)
+            products_dict = dict(
+                (product['id'], product) for product in products_rec)
+            cost_method_product_tmpl_ids = list(
+                set(product['product_tmpl_id'][0]
+                    for product in products_rec
+                    if product['cost_method'] != 'real'))
+            histories = []
+            if cost_method_product_tmpl_ids:
+                cr.execute('SELECT DISTINCT ON'
+                           ' (product_template_id, company_id)'
+                           ' product_template_id, company_id, cost FROM '
+                           'product_price_history WHERE product_template_id '
+                           'in %s AND datetime <= %s ORDER BY '
+                           'product_template_id, company_id, datetime DESC',
+                           (tuple(cost_method_product_tmpl_ids), date))
+                histories = cr.dictfetchall()
+            histories_dict = {}
+            for history in histories:
+                histories_dict[
+                    (history['product_template_id'],
+                     history['company_id'])] = history['cost']
+            for line in res:
+                inv_value = 0.0
+                lines = group_lines.get(str(line.get('__domain', domain)))
+                for line_id in lines:
+                    line_rec = lines_dict[line_id]
+                    product = products_dict[line_rec['product_id']]
+                    if product['cost_method'] == 'real':
+                        price = line_rec['price_unit_on_quant']
+                    else:
+                        price = histories_dict.get(
+                            (product['product_tmpl_id'][0],
+                             line_rec['company_id']), 0.0)
+                    inv_value += price * line_rec['quantity']
+                line['inventory_value'] = inv_value
+                if line['quantity'] == 0.00:
+                    line['price_unit_on_quant'] = 0.00
+                else:
+                    line['price_unit_on_quant'] = inv_value/line['quantity']
 
         if 'fiscal_classification_id' in fields:
             product_obj = self.pool.get('product.product')
